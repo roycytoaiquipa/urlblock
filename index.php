1551676b0704e711b9d13f8e604cc9f171b1da22
yonyyroy1997gmail.com <roycyto1997@gmail.com>
2019-02-06 13:28:16 -0500

    version1

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<title>ATC Multiservicios</title>
+		<link rel="stylesheet" type="text/css" href="css/stylos.css">
+	
+	</head>
+	<body>
+			<div id="principal_prueba">
+			<header>
+				<div id="logo_prueba">
+					<h2 class="titulo_prueba">Pagina Bloqueda..!</h2>
+
+					<p class="sub-titulo">Comunicarse con el administrador gracias.
+						<i>
+							<b> 
+								<u style="color:#0000FF";> 
+										 <font  size=2>amolero@atcmultiservicios.com </font>
+									 <p>	
+									 	 <font size=2>rchipana@atcmultiservicios.com.pe </font>
+									 </p>	 	
+								</u>
+							</b> 
+						</i>
+					</p>		
+				</div>
+				<center >
+         			<img src="img/logo.jpg" width="" height="60">
+    			</center>
+
+			</header>
+				
+				<P ALIGN="CENTER">
+					<IMG SRC= "img/acces-denied.gif">
+					<img src="img/carita.gif" width="90">						
+				</P>
+
+				<section id="slide_prueba">
+					<img src="img/bebe.jpg" width="400" height="350">
+				</section>
+				
+		</div>
+	</body>
+</html>
